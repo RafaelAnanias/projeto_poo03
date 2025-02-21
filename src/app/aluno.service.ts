7b0b68a4d1543e453515630d369f36d2c2e107a9
Lucas Denner Hilláryo da Silva <20241148060038@ifrn.local>
2025-02-21 12:22:51 -0300

    testando listagem

--- src/app/aluno.service.ts
+++ src/app/aluno.service.ts
@@ -8,7 +8,16 @@ import { Turma } from './turma';
 export class AlunoService {
   private alunos: Aluno[] = [];
 
-  constructor() { }
+  constructor() {
+    // Adiciona alunos de exemplo
+    const turma1 = new Turma('Matemática', 101, 'Prof. Silva');
+    const turma2 = new Turma('História', 102, 'Prof. Souza');
+    const turma3 = new Turma('Ciências', 103, 'Prof. Santos');
+
+    this.alunos.push(new Aluno('Alice', 20, turma1));
+    this.alunos.push(new Aluno('Bruno', 22, turma2));
+    this.alunos.push(new Aluno('Carla', 19, turma3));
+  }
 
   adicionarAluno(aluno: Aluno): void {
     this.alunos.push(aluno);
@@ -18,17 +27,3 @@ export class AlunoService {
     return this.alunos;
   }
 }
-
-
-
-
-
-// import { Injectable } from '@angular/core';
-
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class AlunoService {
-
-//   constructor() { }
-// }
